1a208120939c40dfcc2ae2836860f83d352c65a6
zach662 <1295427675@qq.com>
2026-07-11 08:31:40 +0800

    Update Converse2DC3.py

--- improved_modules/Converse2DC3.py
+++ improved_modules/Converse2DC3.py
@@ -11,8 +11,86 @@ class RepC3(nn.Module):
     def forward(self, x):
         return self.cv3(self.m(self.cv1(x)) + self.cv2(x))
 
+
+
+class Converse2D(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, scale=1, padding_mode='circular', eps=1e-5, act=False):
+        super(Converse2D, self).__init__()
+        """
+        Converse2D Operator for Image Restoration Tasks.
+
+        Args:
+            x (Tensor): Input tensor of shape (N, in_channels, H, W), where
+                        N is the batch size, H and W are spatial dimensions.
+            in_channels (int): Number of channels in the input tensor.
+            out_channels (int): Number of channels produced by the operation.
+            kernel_size (int): Size of the kernel.
+            scale (int): Upsampling factor. For example, `scale=2` doubles the resolution.
+            padding_mode (str, optional): Padding method. One of {'reflect', 'replicate', 'circular', 'constant'}.
+                                        Default is `circular`.
+            eps (float, optional): Small value added to denominators for numerical stability.
+                                Default is a small value like 1e-5.
+
+        Returns:
+            Tensor: Output tensor of shape (N, out_channels, H * scale, W * scale), where spatial dimensions
+                    are upsampled by the given scale factor.
+        """
+        
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size =  kernel_size
+        self.scale = scale
+        self.padding = kernel_size - 1
+        self.padding_mode = padding_mode
+        self.eps = eps
+
+
+        # ensure depthwise
+        assert self.out_channels == self.in_channels
+        self.weight = nn.Parameter(torch.randn(1, self.in_channels, self.kernel_size, self.kernel_size))
+        self.bias = nn.Parameter(torch.zeros(1, self.in_channels, 1, 1))
+        self.weight.data = nn.functional.softmax(self.weight.data.view(1,self.in_channels,-1), dim=-1).view(1, self.in_channels, self.kernel_size, self.kernel_size)
+
+        self.act = nn.Identity()
+        if act:
+            self.act = act()
+        
+    def forward(self, x):
+
+        if self.padding > 0:
+            x = nn.functional.pad(x, pad=[self.padding, self.padding, self.padding, self.padding], mode=self.padding_mode, value=0)
+
+        biaseps = torch.sigmoid(self.bias-9.0) + self.eps
+        _, _, h, w = x.shape
+        STy = self.upsample(x, scale=self.scale)
+        if self.scale != 1:
+            x = nn.functional.interpolate(x, scale_factor=self.scale, mode='nearest')
+            # x = nn.functional.interpolate(x, scale_factor=self.scale, mode='bilinear',align_corners=False)
+        # x = torch.zeros_like(x)
+
+        FB = self.p2o(self.weight, (h*self.scale, w*self.scale))
+        FBC = torch.conj(FB)
+        F2B = torch.pow(torch.abs(FB), 2)
+        FBFy = FBC*torch.fft.fftn(STy, dim=(-2, -1))
+        
+        FR = FBFy + torch.fft.fftn(biaseps * x, dim=(-2,-1))
+        x1 = FB.mul(FR)
+        FBR = torch.mean(self.splits(x1, self.scale), dim=-1, keepdim=False)
+        invW = torch.mean(self.splits(F2B, self.scale), dim=-1, keepdim=False)
+        invWBR = FBR.div(invW + biaseps)
+        FCBinvWBR = FBC*invWBR.repeat(1, 1, self.scale, self.scale)
+        FX = (FR-FCBinvWBR) / biaseps
+        out = torch.real(torch.fft.ifftn(FX, dim=(-2, -1)))
+
+        if self.padding > 0:
+            out = out[..., self.padding*self.scale:-self.padding*self.scale, self.padding*self.scale:-self.padding*self.scale]
+
+        return self.act(out)
+
+
+
 class Converse2DC3(RepC3):
     def __init__(self, c1, c2, n=3, e=1):
         super().__init__(c1, c2, n, e)
-        c_ = int(c2 * e)  # hidden channels
+        c_ = int(c2 * e) 
         self.m = nn.Sequential(*[Converse2D(c_, c_, 3) for _ in range(n)])
